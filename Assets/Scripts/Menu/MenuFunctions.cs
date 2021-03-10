d1d8dd1aa63889c1729ffaf3a68341a5f3a8af87
Ruche3D <aymeric.henard@imt-altantique.net>
2021-03-10 16:14:48 +0100

    Creating menu script and adding visor activation function

---
+++ Assets/Scripts/Menu/MenuFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFunctions : MonoBehaviour
+{
+    [Header("The beekeper visor who need to be disable/enable")]
+    [SerializeField]
+    private GameObject visor;  //Peut être le transformer en tableau
+    public void setVisor()
+    {
+        if(visor!=null)
+        {
+            visor.SetActive(!visor.activeSelf);
+        }
+    }
+}
