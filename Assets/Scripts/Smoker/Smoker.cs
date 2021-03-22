1e8d582bafac5f41084ad26d740646622c637c44
Ruche3D <istreon@gmail.com>
2021-03-22 11:49:44 +0100

    Adding smoker's ability to release smoke regulalry

---
+++ Assets/Scripts/Smoker/Smoker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smoker : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject smokePrefab;
+    [SerializeField]
+    private Transform smokeStartPosition;
+
+
+    private float delay = 1.0f;
+    private float time = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time>delay)
+        {
+            time = 0.0f;
+            GameObject smoke= Instantiate(smokePrefab, smokeStartPosition.position, smokeStartPosition.rotation);
+            smoke.transform.parent = smokeStartPosition.transform;
+        }
+    }
+}
