6bc9d81fabcbb1a44da7564dc1148015c2b0fde2
Istreon <47358771+Istreon@users.noreply.github.com>
2021-04-02 15:38:35 +0200

    Adding bee sound

---
+++ Assets/Scripts/BeeFakeBehaviour/BeeSoundBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeSoundBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource beeSound;
+
+
+    bool playing = false;
+    float timer;
+    float delay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        beeSound.spatialize = true;
+        beeSound.Stop();
+        timer = 0;
+        delay = Random.value*1.25f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!playing)
+        {
+            timer += Time.deltaTime;
+            if (timer > delay)
+            {
+                beeSound.Play();
+                playing = true;
+            }
+        }
+
+    }
+}
