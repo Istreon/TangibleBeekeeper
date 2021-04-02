3685f6e7656734d416a4e9daa9a0c39d9a1212ac
Istreon <47358771+Istreon@users.noreply.github.com>
2021-04-02 11:18:57 +0200

    Updating bee behaviour script

--- Assets/Scripts/BeeFakeBehaviour/BeeFakeBehaviourRigidbody.cs
+++ Assets/Scripts/BeeFakeBehaviour/BeeFakeBehaviourRigidbody.cs
@@ -19,37 +19,52 @@ public class BeeFakeBehaviourRigidbody : MonoBehaviour
     private Rigidbody beeBody;
 
 
+    private Vector3 directionVector;
+
+
+    private float delay=3.0f;
+    private float time;
+
+
     // Start is called before the first frame update
     void Start()
     {
         nextObjective = this.transform.position;
+        updateNextObjective();
         beeBody = GetComponent<Rigidbody>();
+        time = 0;
  
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, nextObjective) > 0.05f)
-        {
 
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        time += Time.deltaTime;
+        if (Vector3.Distance(transform.position, nextObjective) > 0.05f && time<delay)
+        {
+            this.transform.LookAt(this.transform.position + beeBody.velocity);
+            beeBody.velocity = directionVector * beeSpeed;
         }
         else
         {
             updateNextObjective();
         }
-
-
     }
 
     void updateNextObjective()
     {
+        time=0;
+        delay = Random.value * 10.0f + 3.0f;
         Vector3 temp = Random.insideUnitCircle * diameter;
         nextObjective = new Vector3(temp.x + areaOrigin.position.x, areaOrigin.position.y + Random.value * height * 2 - height, areaOrigin.position.z + temp.y);
-
-        Vector3 velocityVector = nextObjective.normalized*beeSpeed;
-        this.transform.LookAt(nextObjective);
-
-        beeBody.velocity = velocityVector;
+        //this.transform.LookAt(nextObjective);
+        directionVector = (nextObjective - this.transform.position).normalized;
     }
 }
