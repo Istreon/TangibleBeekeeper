86484aa0685974eba40848ea91fff1c7c873cd02
Ruche3D <istreon@gmail.com>
2021-07-09 12:05:34 +0200

    Updating dectection scripts
    -Updating tracking objects ID
    -It's now possible to know is the roof is hiding the inside of the hive
    -It will be soon possible to know if frames are well positionned in the hive, and get values.

---
+++ Assets/Scripts/AboveHiveDetection.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboveHiveDetection : MonoBehaviour
+{
+    //2 pts
+    //0.75 m
+
+    [SerializeField]
+    private int id=-1;
+    [SerializeField]
+    private GameObject[] hiveOutline;
+
+    [SerializeField]
+    private GameObject[] checkedObjectOutline;
+
+    private int lastNbOfIncludedPoints = 0;
+
+    public void setId(int newID){
+        id=newID;
+    }
+
+  
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        int nb = howManyPointAreIncluded();
+        float dist = getAverageDistanceY();
+        if(nb!=lastNbOfIncludedPoints)
+        {
+            lastNbOfIncludedPoints = nb;
+            switch (nb)
+            {
+                case 0:
+                    Debug.Log("Frame : "+id+" is not above the hive");
+                    break;
+
+                default:
+                    Debug.Log("Frame : " + id + ". Only nb : " + nb + " points are well positioned .... dist = "+ dist);
+                    break;
+            }
+        }
+       
+    }
+
+    public bool objectHideInsideOfTheHive()
+    {
+        bool result = false;
+
+        int nb = howManyPointAreIncluded();
+        float dist = getAverageDistanceY();
+
+        if (nb>=2 && dist<0.75)
+        {
+            result = true;
+        }
+
+        return result;
+    }
+
+    private float getAverageDistanceY()
+    {
+        float heightObject = 0.0f;
+        float heightHive = 0.0f;
+        float result;
+        int count = 0;
+
+        //Get average height of the tested object
+        foreach(GameObject g in checkedObjectOutline)
+        {
+            if(g!=null)
+            {
+                heightObject += g.transform.position.y;
+                count++;
+            }
+        }
+        heightObject = heightObject /count;
+
+
+        //Get average height of the hive outline
+        count = 0;
+        foreach(GameObject g in hiveOutline)
+        {
+            if (g != null)
+            {
+                heightHive += g.transform.position.y;
+                count++;
+            }
+        }
+        heightHive = heightHive / count;
+
+
+        //Get distance between both average height
+
+        result = heightObject - heightHive;
+
+        return result;
+    }
+
+    private int howManyPointAreIncluded()
+    {
+        int res = 0;
+        for(int i=0;i< checkedObjectOutline.Length;i++)
+        {
+            if (pointIsIncluded(checkedObjectOutline[i].transform.position)) res++;
+        }
+        return res;
+    }
+
+
+    private bool pointIsIncluded(Vector3 pointToTest)
+    {
+        //Test if the point is below the hive 
+        float a = hiveOutline[0].transform.up.x;
+        float b = hiveOutline[0].transform.up.y;
+        float c = hiveOutline[0].transform.up.z;
+        float d = -(a * hiveOutline[0].transform.position.x + b * hiveOutline[0].transform.position.y + c * hiveOutline[0].transform.position.z);
+
+        float dist = a * pointToTest.x + b * pointToTest.y + c * pointToTest.z + d;
+        if (dist < 0)
+        {
+            return false;
+        }
+
+        //Test if the point is between the points forming the outline of the hive
+        for (int i=0;i<hiveOutline.Length;i++)
+        {
+            int nextI = (i + 1) % hiveOutline.Length;
+
+            Vector3 vec1 = hiveOutline[i].transform.position - pointToTest;
+            vec1 = new Vector3(vec1.x, 0, vec1.z);
+            Vector3 vec2 = hiveOutline[nextI].transform.position - pointToTest;
+            vec2 = new Vector3(vec2.x, 0, vec2.z);
+            //float angle=Vector3.Angle(vec1, vec2);
+            float angle = Vector3.SignedAngle(vec2, vec1, Vector3.up);
+            if (angle<0) return false;
+        }
+        return true;
+    }
+
+ }
---
+++ Assets/Scripts/FramePositioningDetection.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FramePositioningDetection : MonoBehaviour
+{
+  
+    [SerializeField]
+    private int id;
+
+    [SerializeField]
+    private float sensibility=0.04f;
+
+
+    [Header("GameObject from hive")]
+    [SerializeField]
+    private GameObject[] frameDetectionVolume;
+
+    [SerializeField]
+    private GameObject[] framePositionReferenceInHive;
+
+    [SerializeField]
+    private GameObject[] framePositionReference;
+
+
+    private bool[] wellPositionedAt = { false, false, false };
+
+    private bool tidy = false; //true if honeycomb his well stored in the hive
+
+    private float lastAverageDist = 0;
+
+    private int distPrecision=2;
+
+    private bool tidyButIncorrect = false; //Used to stop message spam
+
+ 
+    void FixedUpdate()
+    {
+        isStillTidy();
+
+    }
+
+
+    public void setId(int newID)
+    {
+        id = newID;
+    }
+
+    /*
+     * Check if the object is still tidy in a storage area of the hive, by calculate distance between them
+     * if not, pass "tidy" attribut to false
+     */
+    private void isStillTidy()
+    {
+        bool res = true;
+        //Check if the wood frame touches all detection areas
+        foreach(bool b in wellPositionedAt)
+        {
+            if (!b) res = false;
+
+        }
+
+        //Reset values
+        for(int i=0;i<wellPositionedAt.Length;i++)
+        {
+            wellPositionedAt[i] = false;
+        }
+
+
+        //If the wood frame touches all detection areas, now check if it well positionned
+        float standardDeviation=0;
+        float averageDist=0;
+        if(res)
+        {
+
+            //Calculate 3 distances between reference points on frame and reference points in hive
+            float distDown = Vector3.Distance(framePositionReferenceInHive[2].transform.position, framePositionReference[2].transform.position);
+
+            float distUp1 = Vector3.Distance(framePositionReferenceInHive[0].transform.position, framePositionReference[0].transform.position);
+            distUp1 = Mathf.Min(distUp1, Vector3.Distance(framePositionReferenceInHive[1].transform.position, framePositionReference[0].transform.position));
+
+            float distUp2 = Vector3.Distance(framePositionReferenceInHive[0].transform.position, framePositionReference[1].transform.position);
+            distUp2 = Mathf.Min(distUp2, Vector3.Distance(framePositionReferenceInHive[1].transform.position, framePositionReference[1].transform.position));
+
+            //Calculate standard deviation and average dist
+            averageDist = (distDown + distUp1 + distUp2) / 2;
+            standardDeviation = Mathf.Max(Mathf.Max(distDown, distUp1), distUp2) - Mathf.Min(Mathf.Min(distDown, distUp1), distUp2);
+            averageDist = truncate(averageDist, distPrecision);
+           // if (averageDist != lastAverageDist ) Debug.Log("Distance : " + averageDist);
+            lastAverageDist=averageDist;
+
+        }
+
+        if (tidy && !res)
+        {
+            tidy = false;
+            tidyButIncorrect = false ;
+            Debug.Log("I am " + id + "and I moved");
+        }
+        else if (!tidy && res)
+        {
+            if(standardDeviation<sensibility)
+            {
+                Debug.Log("I am " + id + " and I found an area for me with a distance of " + averageDist + " in the hive");
+                tidy = true;
+                tidyButIncorrect = false;
+            } else
+            {
+                if(!tidyButIncorrect)
+                {
+                    Debug.Log("I am " + id + " and I am in the hive but I am not straight, my positioning is incorrect " + standardDeviation);
+                    tidyButIncorrect = true;
+                }
+
+            }
+
+        }
+    }
+
+
+    /**
+     * truncate a float number keeping only a certain number of decimal
+     **/
+    private float truncate(float val, int nbAfterTheDecimalPoint)
+    {
+        float res = val;
+        float tenPow= Mathf.Pow(10f, (float) nbAfterTheDecimalPoint);
+        res=Mathf.Floor(val * tenPow) / tenPow;
+        return res;
+    }
+   
+
+    private void OnTriggerStay(Collider other)
+    {
+        for (int i = 0; i < frameDetectionVolume.Length; i++)
+        {
+            if (other.Equals(frameDetectionVolume[i].GetComponent<Collider>()))
+            {
+                wellPositionedAt[i] = true;
+            }
+        }
+    }
+
+}
