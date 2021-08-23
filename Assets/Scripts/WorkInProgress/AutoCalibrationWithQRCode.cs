ad206b064ebdb95923a7b9acd3f7de7c2b95ab81
Istreon <47358771+Istreon@users.noreply.github.com>
2021-08-23 11:01:41 +0200

    Updating script for calibration with tangible object

---
+++ Assets/Scripts/WorkInProgress/AutoCalibrationWithQRCode.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCalibrationWithQRCode : MonoBehaviour
+{
+    #region Serialized fields
+    [SerializeField]
+
+    private GameObject objectToMove;
+    [SerializeField]
+    private Transform QRCodeLinked;
+    #endregion
+
+
+    #region Private fields
+    private Vector3 lastPosition;
+    private Quaternion lastRotation;
+
+    private GameObject parentObject;
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (QRCodeLinked == null)
+        {
+            Debug.LogError("QRCode reference missing ", this);
+            this.enabled = false;
+        }
+        if (objectToMove == null)
+        {
+            Transform parent = gameObject.transform.parent;
+            if (parent == null)
+            {
+                Debug.LogError("This gameobject must have a parent  ", this);
+                this.enabled = false;
+
+            }
+            else
+            {
+                parentObject = parent.gameObject;
+            }
+        }
+        else
+        {
+            parentObject = objectToMove;
+        }
+
+        lastPosition = this.transform.position;
+        lastRotation = this.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (lastPosition != QRCodeLinked.position || lastRotation != QRCodeLinked.rotation)
+        {
+            //Update Position
+            parentObject.transform.position += QRCodeLinked.position - transform.position;
+            lastPosition = QRCodeLinked.position;
+
+            //Update Rotation
+            Quaternion diff = QRCodeLinked.rotation * Quaternion.Inverse(transform.rotation);
+            Vector3 temp = Vector3.zero;
+            temp.y = diff.eulerAngles.y;
+            diff = Quaternion.Euler(temp);
+            parentObject.transform.rotation = diff * parentObject.transform.rotation;
+            lastRotation = QRCodeLinked.rotation;
+        }
+    }
+    #endregion
+}
